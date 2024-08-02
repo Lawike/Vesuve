d4fb931e874475842d969db4de88edd1af4c4e2c
Lawike <loic.gaillard69gmail.com>
2024-08-02 17:53:26 +0200

    feat(Image): add a VesuveImage class to manage vulkan images.

---
+++ src/VesuveImage.hpp
@@ -0,0 +1,30 @@
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#endif
+
+#include <cstdint>
+
+class VesuveImage {
+public:
+	uint32_t width;
+	uint32_t height;
+	uint32_t mipLevels;
+	VkSampleCountFlagBits numSamples;
+	VkFormat format;
+	VkImageTiling tiling;
+	VkImageUsageFlags usage;
+	VkMemoryPropertyFlags properties;
+	VkImage image{};
+	VkDeviceMemory imageMemory{};
+
+	VesuveImage() = default;
+	VesuveImage(VkPhysicalDevice physicalDevice, VkDevice device, uint32_t width, uint32_t height, uint32_t mipLevels, VkSampleCountFlagBits numSamples, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties);
+	void transitionImageLayout(VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels, VkCommandPool commandPool, VkQueue graphicsQueue);
+
+
+private:
+	VkPhysicalDevice physicalDevice;
+	VkDevice device;
+	void createImage();
+};
