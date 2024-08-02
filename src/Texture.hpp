863de3b3df9ae6425ad65b5ecaf0f17ec4ebef1e
Lawike <loic.gaillard69gmail.com>
2024-08-02 17:52:40 +0200

    feat(Texture): add a Texture class to store and create textures.
    It allows to create a texture that can be loaded into the vulkan rendere, if provided a pointer that contains all the image pixels.

---
+++ src/Texture.hpp
@@ -0,0 +1,42 @@
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#endif
+
+#ifndef STB_IMAGE_H
+#define STB_IMAGE_H
+#include "stb_image.h"
+#endif // !STB_IMAGE_H
+
+#include <string>
+
+#include "VesuveImage.hpp"
+
+class Texture {
+public:
+	VesuveImage textureImage;
+	int width;
+	int height;
+	int channels;
+	stbi_uc* pixels;
+	uint32_t mipLevels;
+	VkImageView textureImageView;
+	VkSampler textureSampler;
+	std::string texturePath;
+
+	Texture() = default;
+	Texture(VkPhysicalDevice physicalDevice, VkDevice device, int width, int height, int channels, stbi_uc* pixels, VkCommandPool commandPool, VkQueue graphicsQueue);
+
+private:
+	VkPhysicalDevice physicalDevice;
+	VkDevice device;
+	VkCommandPool commandPool;
+	VkQueue graphicsQueue;
+
+	void createTextureImage();
+	void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
+	void generateMipmaps(VkImage image, VkFormat imageFormat);
+
+
+
+};
